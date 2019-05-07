5de16b26e4c63fc2417079a9bc27d1d074d8ba06
Bob_chemist <bob_chemist@mail.ru>
2019-05-07 21:49:48 +0300

    useContext hook applied

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const ContextUser = React.createContext();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const username = 'Bob';
+
+ReactDOM.render(
+  <ContextUser.Provider value={username}>
+    <App />
+  </ContextUser.Provider>,
+  document.getElementById('root')
+);
+
 serviceWorker.unregister();
