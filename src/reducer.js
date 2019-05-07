f559e1f1d6f468926e1a80fc5a63e6e20a0d2115
Bob_chemist <bob_chemist@mail.ru>
2019-05-08 00:47:20 +0300

    Adding new todos is available

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,18 @@
+import uuidv4 from 'uuid/v4';
+
 export default function todosReducer(state, action) {
   switch (action.type) {
+    case 'ADD_TODO':
+      const newTodo = {
+        id: uuidv4(),
+        text: action.payload,
+        complete: false,
+      };
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+      };
+
     case 'TOGGLE_TODO':
       const toggledTodos = state.todos.map(todo =>
         todo.id === action.payload.id
