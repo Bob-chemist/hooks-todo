01bf0cbe3096247ccf6455a863795297467d9702
Bob_chemist <bob_chemist@mail.ru>
2019-05-08 00:08:46 +0300

    Removing todos added, Done/undone toggle is added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,7 +14,13 @@ function TodoList() {
             key={todo.id}
             className="flex items-center bg-orange-dark border black border-dashed border-2 my-2 py-2"
           >
-            <span onDoubleClick={''} className="flex-1 ml-12cursor-pointer">
+            <span
+              onDoubleClick={() =>
+                dispatch({ type: 'TOGGLE_TODO', payload: todo })
+              }
+              className={`flex-1 ml-12 cursor-pointer ${todo.complete &&
+                'line-through text-grey-darkest'}`}
+            >
               {todo.text}
             </span>
             <button className="">
@@ -24,7 +30,9 @@ function TodoList() {
                 className="h-6"
               />
             </button>
-            <button className="">
+            <button
+              onClick={() => dispatch({ type: 'REMOVE_TODO', payload: todo })}
+            >
               <img
                 src="https://icon.now.sh/delete/8b0000"
                 alt="Delete Icon"
--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,24 @@
 export default function todosReducer(state, action) {
   switch (action.type) {
-    case 'g':
-      break;
+    case 'TOGGLE_TODO':
+      const toggledTodos = state.todos.map(todo =>
+        todo.id === action.payload.id
+          ? { ...action.payload, complete: !action.payload.complete }
+          : { ...todo }
+      );
+      return {
+        ...state,
+        todos: toggledTodos,
+      };
 
+    case 'REMOVE_TODO':
+      const removeTodos = state.todos.filter(
+        todo => todo.id !== action.payload.id
+      );
+      return {
+        ...state,
+        todos: removeTodos,
+      };
     default:
       return state;
   }
