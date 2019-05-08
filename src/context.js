39d1fea7fa41eb1a58cee9ea285bd2fd4b598db8
Bob_chemist <bob_chemist@mail.ru>
2019-05-08 18:19:46 +0300

    Custom hook added to fetch data from server

--- src/context.js
+++ src/context.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const TodosContext = React.createContext({
   todos: [
-    { id: 1, text: 'Eat', complete: false },
-    { id: 2, text: 'Drink', complete: false },
-    { id: 3, text: 'Sleep', complete: true },
+    // { id: 1, text: 'Eat', complete: false },
+    // { id: 2, text: 'Drink', complete: false },
+    // { id: 3, text: 'Sleep', complete: true },
   ],
   currentTodo: {},
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useState, useEffect, useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import TodosContext from './context';
 import todosReducer from './reducer';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import Axios from 'axios';
+
+const useAPI = endpoint => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+    //eslint-disable-next-line
+  }, []);
+
+  const getData = async () => {
+    const response = await Axios.get(endpoint);
+    setData(response.data);
+  };
+
+  return data;
+};
 
 const App = () => {
   const initialState = useContext(TodosContext);
   const [state, dispatch] = useReducer(todosReducer, initialState);
+  const savedTodos = useAPI(
+    'https://hooks-todo-9b98c.firebaseio.com/todos.json'
+  );
+
+  useEffect(() => {
+    dispatch({
+      type: 'GET_TODOS',
+      payload: savedTodos,
+    });
+  }, [savedTodos]);
 
   return (
     <TodosContext.Provider value={{ state, dispatch }}>
